db4fd27c80366ec72dbc0fba4cc4c2984895633b
Mike <michellchristofferson@gmail.com>
2022-08-21 10:12:41 -0300

    Update mobile-navbar.js

---
+++ js/mobile-navbar.js
@@ -0,0 +1,25 @@
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelector(navLinks);
+        this.activeClass = "active";
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", () => console.log("Hey man"));
+    }
+
+    init() {
+        if(this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const MobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
