300f7c8e4d6d79953a72f7b6014a16381eecdba1
Mike <michellchristofferson@gmail.com>
2022-08-21 22:04:46 -0300

    botão responsivo

--- js/mobile-navbar.js
+++ js/mobile-navbar.js
@@ -4,6 +4,22 @@ class MobileNavbar {
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelector(navLinks);
         this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
     }
 
     addClickEvent() {
